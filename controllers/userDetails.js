0bd0368e34ce977dd6492abd75351897f2127c7b
rajnish135 <rajnishrajyan@gmail.com>
2025-04-20 17:35:42 +0530

    dddvdvdvdnjj

--- controllers/userDetails.js
+++ controllers/userDetails.js
@@ -1,22 +1,32 @@
-const getUserDetailsFromToken = require("../helpers/getUserDetailsFromToken")
+const getUserDetailsFromToken = require("../helpers/getUserDetailsFromToken");
 
-async function userDetails(request,response){
-    try {
-        const token = request.cookies.token || ""
-        
-        const user = await getUserDetailsFromToken(token)
+async function userDetails(request, response) {
+  try {
+    // Try getting token from Authorization header
+    const authHeader = request.headers.authorization || "";
+    let token = authHeader.startsWith("Bearer ")
+      ? authHeader.split(" ")[1]
+      : "";
 
-        return response.status(200).json({
-            message : "user details",
-            data : user
-        })
-    } 
-    catch (error) {
-        return response.status(500).json({
-            message : error.message || error,
-            error : true
-        })
+    // If not found in header, try from cookies
+    if (!token && request.cookies?.token) {
+      token = request.cookies.token;
     }
+
+    // Use token
+
+    const user = await getUserDetailsFromToken(token);
+
+    return response.status(200).json({
+      message: "user details",
+      data: user,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+    });
+  }
 }
 
-module.exports = userDetails
+module.exports = userDetails;
