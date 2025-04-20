811d04748d3afcede7a03fddfd8be1347638e9a0
rajnish135 <rajnishrajyan@gmail.com>
2025-04-20 16:52:51 +0530

    dd

--- socket/index.js
+++ socket/index.js
@@ -12,7 +12,7 @@ const getConversation = require("../helpers/getConversation.js")
 const app = express();
 
 app.use(cors({
-   origin: process.env.FRONTEND_URL, 
+   origin: [process.env.FRONTEND_URL, "http://localhost:5173"], 
    credentials: true 
  }));
 
